ae0d19d188b2ec6c20da4ec9f3e8cb2c18d8792f
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2022-09-03 14:20:14 +0530

    routes get and post

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require ("express");
 const app= express();
 
+// routes
 app.get("/",(req,res)=> {
   res.send("hellw world");
 });
+
+app.get('/posts',(req,res)=> {
+  res.send('we are on posts')
+})
 
 // port
 app.listen(3000);
